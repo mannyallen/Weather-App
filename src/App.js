7b995683371e3746a4c7e704a5b07ee540e8a3e3
mannyallen <immanuelmallen@gmail.com>
2020-04-23 16:53:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const api ={
+  key: '0cf7838ca23d9afb24e138de0dce0293',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Enter a ZIP Code"
+          ></input>
+        </div>
+      </main>
     </div>
   );
 }
